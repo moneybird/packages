790ef9f2595ee19594d8b4c15dee5033f69e21e4
Hugo Olthof <hugo@moneybird.com>
2023-02-25 19:59:32 +0100

    Uses mocks generated with latest stable Flutter version

--- packages/webview_flutter/webview_flutter_android/test/android_webview_controller_test.mocks.dart
+++ packages/webview_flutter/webview_flutter_android/test/android_webview_controller_test.mocks.dart
@@ -1093,11 +1093,11 @@ class MockAndroidWebViewWidgetCreationParams extends _i1.Mock
 class MockExpensiveAndroidViewController extends _i1.Mock
     implements _i7.ExpensiveAndroidViewController {
   @override
-  int get textureId => (super.noSuchMethod(
-        Invocation.getter(#textureId),
-        returnValue: 0,
-        returnValueForMissingStub: 0,
-      ) as int);
+  bool get requiresViewComposition => (super.noSuchMethod(
+        Invocation.getter(#requiresViewComposition),
+        returnValue: false,
+        returnValueForMissingStub: false,
+      ) as bool);
   @override
   int get viewId => (super.noSuchMethod(
         Invocation.getter(#viewId),
@@ -1153,11 +1153,18 @@ class MockExpensiveAndroidViewController extends _i1.Mock
         returnValueForMissingStub: _i9.Future<void>.value(),
       ) as _i9.Future<void>);
   @override
-  _i9.Future<void> create({_i4.Size? size}) => (super.noSuchMethod(
+  _i9.Future<void> create({
+    _i4.Size? size,
+    _i4.Offset? position,
+  }) =>
+      (super.noSuchMethod(
         Invocation.method(
           #create,
           [],
-          {#size: size},
+          {
+            #size: size,
+            #position: position,
+          },
         ),
         returnValue: _i9.Future<void>.value(),
         returnValueForMissingStub: _i9.Future<void>.value(),
@@ -1438,6 +1445,12 @@ class MockPlatformViewsServiceProxy extends _i1.Mock
 class MockSurfaceAndroidViewController extends _i1.Mock
     implements _i7.SurfaceAndroidViewController {
   @override
+  bool get requiresViewComposition => (super.noSuchMethod(
+        Invocation.getter(#requiresViewComposition),
+        returnValue: false,
+        returnValueForMissingStub: false,
+      ) as bool);
+  @override
   int get viewId => (super.noSuchMethod(
         Invocation.getter(#viewId),
         returnValue: 0,
@@ -1492,11 +1505,18 @@ class MockSurfaceAndroidViewController extends _i1.Mock
         returnValueForMissingStub: _i9.Future<void>.value(),
       ) as _i9.Future<void>);
   @override
-  _i9.Future<void> create({_i4.Size? size}) => (super.noSuchMethod(
+  _i9.Future<void> create({
+    _i4.Size? size,
+    _i4.Offset? position,
+  }) =>
+      (super.noSuchMethod(
         Invocation.method(
           #create,
           [],
-          {#size: size},
+          {
+            #size: size,
+            #position: position,
+          },
         ),
         returnValue: _i9.Future<void>.value(),
         returnValueForMissingStub: _i9.Future<void>.value(),
